361986745e8b0aa22cbc922b7303adc0c961df45
Benedikt Schnatterbeck <benediktms@hey.com>
2021-03-11 00:21:06 +0000

    Add interactors for show and create

---
+++ app/interactors/users/create.rb
@@ -0,0 +1,18 @@
+# frozen_string_literal: true
+
+module Users
+  class Create < BaseInteractor
+    delegate :attributes, to: :context
+
+    def call
+      user = User.create(**attributes)
+
+      if user.persisted?
+        context.user = user
+        context.message = 'You have successfully signed up'
+      else
+        context.fail! errors: user.errors.full_messages, user: nil
+      end
+    end
+  end
+end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,7 +5,7 @@
 #  id              :bigint           not null, primary key
 #  email           :string
 #  first_name      :string
-#  lastname        :string
+#  last_name       :string
 #  password_digest :string
 #  created_at      :datetime         not null
 #  updated_at      :datetime         not null
